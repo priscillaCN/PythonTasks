d672439a2a7746999d870c3e215b8ab02ded5644
priscillaCN <priscillannamani00@gmail.com>
2026-04-18 12:35:09 +0100

    Update tiered_discount.py

--- ClassWork/tiered_discount.py
+++ ClassWork/tiered_discount.py
@@ -1,4 +1,4 @@
-"""A program that appiles tiered discounts"""
+"""A program that appiles tiered discounts to customers' total spending"""
 
 
 """
@@ -13,16 +13,16 @@ if total_spending < 1000:
     print ('no discount')
 
 elif total_spending <= 10000:
-    discount = (total_spending//100) * 5
+    discount = (total_spending / 100) * 5
     new_price = total_spending - discount
     print ('you receive a 5% discount! Your new price is', new_price)
 
 elif total_spending <= 50000:
-    discount = (total_spending//100) * 10
+    discount = (total_spending / 100) * 10
     new_price = total_spending - discount
     print ('you receive a 10% discount! Your new price is', new_price)
 
 else:
-    discount = (total_spending//100) * 20
+    discount = (total_spending / 100) * 20
     new_price = total_spending - discount
     print ('you receive a 20% discount! Your new price is', new_price)
